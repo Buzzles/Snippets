c8ebf6493ae1738a34798441c8a77dd1c2f6fa59
Ian B <ibuswell@gmail.com>
2015-11-24 23:33:27 +0000

    Initial Commit of Examples Project

---
+++ Examples/Examples/Mutables.fs
@@ -0,0 +1,12 @@
+﻿module Mutables
+
+// Imperative Support
+
+// Non mutable, as it's function
+let y = 1
+y <= 2
+
+// Define 'x' with initial value '1'
+let mutable x = 1
+// Change the value of 'x' to '3'
+x <- 3
