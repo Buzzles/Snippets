e81cb0fbc4484195f26f0d77bd53a07d75f609d9
Ian Buswell <ianbuswell@etech.net>
2016-01-21 19:10:16 +0000

    Adding initial broken LotteryNumberPicker

---
+++ LotteryNumberPicker/LotteryNumberPicker/NumberPicker.fs
@@ -0,0 +1,13 @@
+﻿#light
+
+module NumberPicker
+
+type NumberPicker (maxRange, amountOfNumbers) =
+    let numberRange = [1..maxRange]
+    member s.GetNumbers =
+        let outarray =
+        |> let x = [1..amountOfNumbers]    
+        
+        outarray
+    
+
