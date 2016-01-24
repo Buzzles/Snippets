dd7fa5c2829f63c47dc6b4947222b531bb5a82c4
Ian B <ibuswell@gmail.com>
2016-01-24 14:04:46 +0000

    Progess on Number Picket

--- LotteryNumberPicker/LotteryNumberPicker/NumberPicker.fs
+++ LotteryNumberPicker/LotteryNumberPicker/NumberPicker.fs
@@ -3,11 +3,14 @@
 module NumberPicker
 
 type NumberPicker (maxRange, amountOfNumbers) =
-    let numberRange = [1..maxRange]
+    let numberRange = {1..maxRange}
     member s.GetNumbers =
         let outarray =
-        |> let x = [1..amountOfNumbers]    
-        
+            numberRange 
+            |> Seq.map (fun values -> s.GetRandomWithRange(values) )     
+            |> Seq.take amountOfNumbers
         outarray
-    
-
+    member s.GetRandomWithRange (x) =
+        let rng = System.Random()
+        let r = rng.Next(1, maxRange)
+        r
