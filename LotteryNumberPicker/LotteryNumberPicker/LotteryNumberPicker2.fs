8c3ae925c87443fef58d2f35890c15f7ce49f0cc
Ian Buswell <ianbuswell@etech.net>
2016-01-27 13:07:10 +0000

    Adding notes for list sorting algorithm

--- LotteryNumberPicker/LotteryNumberPicker/LotteryNumberPicker2.fs
+++ LotteryNumberPicker/LotteryNumberPicker/LotteryNumberPicker2.fs
@@ -6,11 +6,17 @@ type NumberPicker2 (maxRange, amountOfNumbers) =
     let mutable numberRange = {1..maxRange}
     let results = [1..amountOfNumbers]
     let rng = System.Random()
+    let GetRnd =
+        let r = rng.Next(1, Seq.last numberRange)
+        r
+    let JumbleList = 
+    // Apply Fisher-Yates shuffle / Knuth shuffle (start at 0, generate a random k less than current length, move to index of k, add number to new list and remove from old, repeat.
+        numberRange  
     let GetRandomWithRange (x) =
         let r = rng.Next(1, Seq.last numberRange)
         let b = new List<int>(results)
         let c = b.Remove(x); 
-//        let newResults = results |> Seq.choose  (fun value -> match value with value.Equals(r) == true)
+    //  let newResults = results |> Seq.choose  (fun value -> match value with value.Equals(r) == true)
         r
     member this.GetNumbers =
         let outarray =
