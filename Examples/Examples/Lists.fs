43f65c6a80cb5c1b22f3ed9c178b4bba20f85cc8
Ian Buswell <ianbuswell@etech.net>
2016-05-16 14:12:21 +0100

    Adding List.iter example

--- Examples/Examples/Lists.fs
+++ Examples/Examples/Lists.fs
@@ -1,5 +1,7 @@
 module Lists
 
+open FSharp.Collections
+
 let l1 = [1; 2; 3]
 let l2 = [4; 5]
 
@@ -8,4 +10,13 @@ let conExample =
 
 let appendExample =
     l1 @ l2
+
+// Iter example
+let nop x : unit = 
+    x |> ignore
+    
+let woo li =
+    List.iter (fun x -> nop x) li
+
+// .map example
 
