5241367c693f49dd008b050c9dbb9e029560f912
Ian B <ibuswell@gmail.com>
2016-07-01 00:05:20 +0100

    Add basic list and basic type pattern match

--- Examples/Examples/PatternMatching.fs
+++ Examples/Examples/PatternMatching.fs
@@ -10,6 +10,17 @@ let tst = round(4)
 
 printf "%i" tst
 
+let testM1 x =
+    match x with
+    | h :: t -> printfn "First item %A" h
+    | [] -> printfn "Empty list"
+
+let typeMatch (t : obj) =
+    match t with
+    | :? string -> printfn "t is a string"
+    | :? int -> printfn "t is an int"
+    | _ -> printfn "unknown type"
+
 let moreComplexMatch (x, y) = 
     if x >= 100 || y >= 100 then 100, 100
     elif x < 0 || y < 0 then 0, 0
